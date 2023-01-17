b64ecf7669c6f79a93995db97532955c2de4c4c1
MaximeR12 <rowell.maxime2001@gmail.com>
2023-01-17 16:58:10 +0100

    ajout des fonctions principales non definies

---
+++ utils.py
@@ -0,0 +1,22 @@
+def player_is_alive() -> bool:
+    """Checks if the player's health is > 0
+
+    Returns:
+        bool: True if player's HP >0, else return False
+    """
+    pass
+
+def enemy_is_alive() -> bool:
+    pass
+
+def afficher_status() ->None:
+    pass
+
+def player_attack() -> None:
+    pass
+
+def player_heals() -> None:
+    pass
+
+def ennemi_attacks() -> None:
+    pass
