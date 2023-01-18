0d895090d2ea4b0f7e66aa4d377f00664efd7145
MaximeR12 <rowell.maxime2001@gmail.com>
2023-01-18 09:58:25 +0100

    ajout de player_attack() et player_heals()

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+
 def player_is_alive() -> bool:
     """Checks if the player's health is > 0
 
@@ -7,16 +8,28 @@ def player_is_alive() -> bool:
     pass
 
 def enemy_is_alive() -> bool:
-    pass
+    if rpg_data["enemy_hp"] > 0:
+        return True
+    return False
+    
 
 def afficher_status() ->None:
     pass
 
 def player_attack() -> None:
-    pass
+    rpg_data["enemy_hp"] -= (10 + randint(10))
+    if rpg_data["enemy_hp"] < 0:
+        rpg_data["enemy_hp"] = 0
+    
 
 def player_heals() -> None:
-    pass
+    if rpg_data["potion_number"]>=1:
+        rpg_data["potion_number"]-=1
+        rpg_data["player_hp"]+=(17+randint(8))
+        if rpg_data["player_hp"]>50:
+            rpg_data["player_hp"]=50
+    else:
+        print("You don't have enough potions !")
 
 def ennemys_turn() -> None:
     pass
