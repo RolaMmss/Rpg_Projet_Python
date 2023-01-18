472795faad55cc42c32ba227c66eec3ed5f5fb62
MaximeR12 <rowell.maxime2001@gmail.com>
2023-01-18 14:53:21 +0100

    amelioration majeure, ajout du niveau 2, affichage reactif au enter avant de passer à la suite et ajout des messages entre chaque action

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from utils import player_is_alive, enemy_is_alive, display_status, player_attack, player_heals, ennemys_turn
+from utils import player_is_alive, enemy_is_alive, display_status, player_attack, player_heals, ennemys_turn ,next_level, all_enemies_dead
  # Preparing a game
 player_name = input("Choose a name for your avatar : ")
+phrase = ('')
 rpg_data = {
+    "level" : 1,
     "player_hp" : 50 ,
     "enemy_hp" : 50 ,
-    "potion_number" : 3 ,
+    "potion_number" : 5 ,
     "turn" :  0, 
     "boss_line_1" : " .-.   ",
     "boss_line_2" : "(o o)  ",
@@ -26,26 +28,31 @@ Remember to grab potions BEFORE you attack !''')
 # Player setup #
 
 
-while player_is_alive(rpg_data) and enemy_is_alive(rpg_data):          # Check hp of both player and enemy
+while player_is_alive(rpg_data) and not all_enemies_dead(rpg_data):          # Check hp of both player and enemy
     # Check if enemy is still alive
-    # if enemy_is_alive():
+    if enemy_is_alive(rpg_data):
         # display the status of player : name, hp, number of potions left      
-        display_status(rpg_data, player_name)    
+        display_status(rpg_data, player_name)  
+        if phrase !=None and phrase != '' :
+            print(phrase)
+            input()
         # check which turn
         if rpg_data["turn"]%2 == 0 :   # turn is pair
         # Demand an action from player: Either to attack or to take a potion
             action = input("What do you want to do ? (attack/potion) \n")
             if action.lower() == "attack":
-                player_attack(rpg_data)
+                phrase = player_attack(rpg_data,player_name)
             elif action.lower() == "potion":
-                player_heals(rpg_data)
+                phrase = player_heals(rpg_data,player_name)
             else:
                 print('Veuillez entrer une action valide')
         
         # Enemy's turn
         else:
-            ennemys_turn(rpg_data)
-
+            phrase = ennemys_turn(rpg_data, player_name)
+    
+    else:
+        phrase = next_level(rpg_data)
 
 display_status(rpg_data, player_name)
 print('\nFin du jeu')
--- utils.py
+++ utils.py
@@ -32,40 +32,71 @@ def display_status(rpg_data, player_name) ->None:
         " ",("◊")*rpg_data["potion_number"], "\n",
         "       ", rpg_data["player_line_1"], "                  ", rpg_data["boss_line_1"], "\n",
         "       ", rpg_data["player_line_1"], "                  ", rpg_data["boss_line_2"], "\n",
-        "       ", rpg_data["player_line_1"], "                  ", rpg_data["boss_line_1"], "\n",
+        "       ", rpg_data["player_line_1"], "                  ", rpg_data["boss_line_3"], "\n",
         "       ", rpg_data["player_line_4"], "                  ", rpg_data["boss_line_4"], "\n",
         "       ", rpg_data["player_line_5"], "                  ", rpg_data["boss_line_5"], "\n",
         "       ", rpg_data["player_line_6"], "                  ", rpg_data["boss_line_6"], "\n",
         )
 
-def player_attack(rpg_data) -> None:
+def player_attack(rpg_data, player_name) -> None:
     """if player choosed "attack", enemy_hp decrease randomly by 10-20 HP
     """
-    rpg_data["enemy_hp"] -= (10 + randint(0,10))
+    degats = 15 + randint(0,10)
+    rpg_data["enemy_hp"] -= degats
     if rpg_data["enemy_hp"] < 0:
         rpg_data["enemy_hp"] = 0
     rpg_data["turn"] += 1
+    return f'{player_name} attacks GHOST, -{degats} HP'
     
 
-def player_heals(rpg_data) -> None:
+def player_heals(rpg_data, player_name) -> None:
     """if player choosed "potion", player_hp increase randomly by 17-25 HP potion_number decrease by 1
     """
+    soins = 22+randint(0,8)
     if rpg_data["potion_number"] >= 1:
         rpg_data["potion_number"] -= 1
-        rpg_data["player_hp"] += (22+randint(0,8))
+        rpg_data["player_hp"] += soins
         rpg_data["turn"]+=1
+        
         if rpg_data["player_hp"] > 50:
             rpg_data["player_hp"] = 50
+        return f'{player_name} heals himself, he gains {soins} HP'
     else:
         print("You don't have enough potions !")
 
-def ennemys_turn(rpg_data) -> None:
+def ennemys_turn(rpg_data,player_name) -> None:
     """plays enemy's turn
     player's health should decrease randomly by 5-20 hp
     """
-    print('\nle boss joue\n')
-    rpg_data["player_hp"] -= (10+randint(0,10))
+    degats = 7+randint(0,8)
+    rpg_data["player_hp"] -= (degats)
     rpg_data["turn"]+=1
     if rpg_data["player_hp"] < 0:
         rpg_data["player_hp"]= 0
+    return f'Ghost hit {player_name} ! Ouch ! -{degats} HP'
     
+def all_enemies_dead(rpg_data):
+    max_level = 2
+    if rpg_data["level"] == max_level and not enemy_is_alive(rpg_data): #Test si on est au niveau Max (en l'occurence 2)
+        return True
+    return False
+
+def next_level(rpg_data):
+    rpg_data["level"] += 1
+    
+    if rpg_data["level"] == 2:          #LEVEL 2
+        rpg_data["player_hp"] = 50
+        rpg_data["enemy_hp"] = 75
+        rpg_data["turn"] = 0
+        rpg_data["boss_name"] = "" 
+        rpg_data["boss_line_1"] = "  <=======]}======" #18 caractères de large
+        rpg_data["boss_line_2"] = "    --.   /|      "
+        rpg_data["boss_line_3"] = "   _\"/_.'/       "
+        rpg_data["boss_line_4"] = " .'._._,.'        "
+        rpg_data["boss_line_5"] = " :/ \{}/          "
+        rpg_data["boss_line_6"] = "(L  /--',----._   "
+        rpg_data["boss_line_7"] = "    |          \\ "
+        rpg_data["boss_line_8"] = "   : /-\ .'-'\ / |"
+        rpg_data["boss_line_9"] = "    \\, ||    \|  "
+        rpg_data["boss_line_10"] ="     \/ ||    ||  "
+        return 'CENTAUR INCOMING !!!'
