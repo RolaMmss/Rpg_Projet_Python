be7d22067b2aedcd8fd17b42162ee90128c2dd2f
RolaMmss <rola.el-moallem@hotmail.fr>
2023-01-18 12:02:23 +0100

    add tests for player_is_alive and enemy_is_alive

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 
-import random
-
- # Preparing a game
-player_name = input("Choose a name for your avatar : ")
-print("Welcome "+ player_name +". You will face off against the computer!")
-print()
-print('''In this battle you and the computer will take turns battling, the first to fall loses!
-There are potions laying around for you to grab! You may use up to three potions!
-Remember to grab potions BEFORE you attack !''')
+import random as rd
+from utils import *
 
 # Player setup #
 rpg_data = {
@@ -17,25 +10,35 @@ rpg_data = {
     "turn" :  0 
 }
 
-while player_is_alive() and enemy_is_alive():          # Check hp of both player and enemy  
+ # Preparing a game
+player_name = input("Choose a name for your avatar : ")
+print("Welcome "+ player_name +". You will face off against the computer!")
+print()
+print('''In this battle you and the computer will take turns battling, the first to fall loses!
+There are potions laying around for you to grab! You may use up to three potions!
+Remember to grab potions BEFORE you attack !''')
+
+
+
+while player_is_alive(rpg_data) and enemy_is_alive(rpg_data):          # Check hp of both player and enemy  
     # Check if enemy is still alive
     # if enemy_is_alive():
         
         # display the status of player : name, hp, number of potions left      
-        afficher_status()    
+        afficher_status(rpg_data)    
         # check which turn
         if rpg_data["turn"]%2 == 0 :   # turn is pair
         
         # Demand an action from player: Either to attack or to take a potion
-            action = input("Attack or potion")
+            action = input("Attack or potion \n")
             if action == "attack":
-                player_attack()
+                player_attack(rpg_data)
             elif action == "potion":
-                player_heals()
+                player_heals(rpg_data)
             #switch_turn
             rpg_data["turn"] += 1
         
 
         else:
-            enemy_tun()
+            enemy_turn()
             rpg_data["turn"]+=1
--- utils.py
+++ utils.py
@@ -1,15 +1,17 @@
 
-def player_is_alive() -> bool:
+import random 
+
+def player_is_alive(rpg_data) -> bool:
     """Checks if the player's HP is > 0
 
     Returns:
         bool: True if player's HP >0, else return False
     """
     if rpg_data["player_hp"] > 0:
-        return True
+        return True# Player setup #
     return False
 
-def enemy_is_alive() -> bool:
+def enemy_is_alive(rpg_data) -> bool:
     """checks if the enemy's HP is > 0
 
     Returns:
@@ -20,10 +22,12 @@ def enemy_is_alive() -> bool:
     return False
     
 
-def afficher_status() ->None:
-    pass
+def afficher_status(rpg_data) ->None:
+    
+    return rpg_data
 
-def player_attack() -> None:
+
+def player_attack(rpg_data) -> None:
     """if player choosed "attack", enemy_hp decrease randomly by 10-20 HP
     """
     rpg_data["enemy_hp"] -= (10 + randint(10))
@@ -31,21 +35,21 @@ def player_attack() -> None:
         rpg_data["enemy_hp"] = 0
     
 
-def player_heals() -> None:
+def player_heals(rpg_data) -> None:
     """if player choosed "potion", player_hp increase randomly by 17-25 HP potion_number decrease by 1
     """
     if rpg_data["potion_number"] >= 1:
         rpg_data["potion_number"] -= 1
-        rpg_data["player_hp"] += (17+randint(8))
+        rpg_data["player_hp"] += (17 + randint(8))
         if rpg_data["player_hp"] > 50:
             rpg_data["player_hp"] = 50
     else:
         print("You don't have enough potions !")
 
-def ennemys_turn() -> None:
+def enemy_turn(rpg_data) -> None:
     """plays enemy's turn
     player's health should decrease randomly by 5-20 hp
     """
-    rpg_data["player_hp"] -= (5+randint(15))
+    rpg_data["player_hp"] -= (5 + randint(15))
     if rpg_data["player_hp"] < 0:
         rpg_data["player_hp"]= 0
