dc333fa720a3a80985ba01d48cc7220d126954dc
MaximeR12 <rowell.maxime2001@gmail.com>
2023-01-18 15:59:01 +0100

    fix les problemes d'affichage du centaur, change les noms, rajoute des attaques differentes au centaur

--- utils.py
+++ utils.py
@@ -27,8 +27,8 @@ def display_status(rpg_data, player_name) ->None:
     Display characters with names, healths, and potions
     """
     print(" ",
-        player_name, "                           ", rpg_data["boss1"], "\n",
-        " HP : ", rpg_data["player_hp"], "/ 50                      HP : ", rpg_data["enemy_hp"], "/ 50","\n",
+        player_name, "                           ", rpg_data["boss_name"], "\n",
+        " HP : ", rpg_data["player_hp"], "/ 50                      HP : ", rpg_data["enemy_hp"], "/ ",rpg_data["enemy_max_hp"],"\n",
         " ",("◊")*rpg_data["potion_number"], "\n",
         "       ", rpg_data["player_line_1"], "                  ", rpg_data["boss_line_1"], "\n",
         "       ", rpg_data["player_line_2"], "                  ", rpg_data["boss_line_2"], "\n",
@@ -42,7 +42,7 @@ def display_status(rpg_data, player_name) ->None:
         "       ", rpg_data["player_line_10"], "                  ", rpg_data["boss_line_10"], "\n",
         )
 
-def player_attack(rpg_data, player_name) -> None:
+def player_attack(rpg_data, player_name) -> str:
     """if player choosed "attack", enemy_hp decrease randomly by 10-20 HP
     """
     degats = 15 + randint(0,10)
@@ -53,10 +53,10 @@ def player_attack(rpg_data, player_name) -> None:
     return f'{player_name} attacks GHOST, -{degats} HP'
     
 
-def player_heals(rpg_data, player_name) -> None:
+def player_heals(rpg_data, player_name) -> str:
     """if player choosed "potion", player_hp increase randomly by 17-25 HP potion_number decrease by 1
     """
-    soins = 22+randint(0,8)
+    soins = 30+randint(0,10)
     if rpg_data["potion_number"] >= 1:
         rpg_data["potion_number"] -= 1
         rpg_data["player_hp"] += soins
@@ -68,18 +68,41 @@ def player_heals(rpg_data, player_name) -> None:
     else:
         print("You don't have enough potions !")
 
-def ennemys_turn(rpg_data,player_name) -> None:
+def ennemys_turn(rpg_data,player_name) -> str:
     """plays enemy's turn
     player's health should decrease randomly by 5-20 hp
     """
-    degats = 7+randint(0,8)
-    rpg_data["player_hp"] -= (degats)
-    rpg_data["turn"]+=1
-    if rpg_data["player_hp"] < 0:
-        rpg_data["player_hp"]= 0
-    return f'Ghost hit {player_name} ! Ouch ! -{degats} HP'
     
-def all_enemies_dead(rpg_data):
+    if rpg_data["level"] == 1:
+        degats = 7+randint(0,8)
+        rpg_data["player_hp"] -= (degats)
+        rpg_data["turn"]+=1
+        if rpg_data["player_hp"] < 0:
+            rpg_data["player_hp"]= 0
+        return f'{rpg_data["boss_name"]} hit {player_name} ! Ouch ! -{degats} HP'
+
+    elif rpg_data["level"] == 2:
+        choix = randint(0,4)
+        if choix == 2:
+            degats = 7 + randint(0,3)
+            rpg_data["player_hp"] -= degats
+            rpg_data["enemy_hp"] += degats
+            rpg_data["turn"] += 1
+            if rpg_data["player_hp"] < 0:
+                rpg_data["player_hp"]= 0
+            if rpg_data["enemy_hp"] > 75:
+                rpg_data["enemy"]= 75
+            return f'{rpg_data["boss_name"]} steals {degats} HP from {player_name}'
+        else:
+            degats = 12+randint(0,6)
+            rpg_data["player_hp"] -= degats
+            rpg_data["turn"]+=1
+            if rpg_data["player_hp"] < 0:
+                rpg_data["player_hp"]= 0
+            return f'{rpg_data["boss_name"]} hit {player_name} with his spear ! -{degats} HP'
+
+    
+def all_enemies_dead(rpg_data) -> bool:
     max_level = 2
     if rpg_data["level"] == max_level and not enemy_is_alive(rpg_data): #Test si on est au niveau Max (en l'occurence 2)
         return True
@@ -91,16 +114,17 @@ def next_level(rpg_data):
     if rpg_data["level"] == 2:          #LEVEL 2
         rpg_data["player_hp"] = 50
         rpg_data["enemy_hp"] = 75
+        rpg_data["enemy_max_hp"] = 75
         rpg_data["turn"] = 0
         rpg_data["boss_name"] = "Charles" 
         rpg_data["boss_line_1"] = "  <=======]}======" #18 caractères de large
         rpg_data["boss_line_2"] = "    --.   /|      "
-        rpg_data["boss_line_3"] = "   _\"/_.'/       "
+        rpg_data["boss_line_3"] = """   _ "/_.'/       """
         rpg_data["boss_line_4"] = " .'._._,.'        "
         rpg_data["boss_line_5"] = " :/ \{}/          "
         rpg_data["boss_line_6"] = "(L  /--',----._   "
-        rpg_data["boss_line_7"] = "    |          \\ "
+        rpg_data["boss_line_7"] = "    |          \\\ "
         rpg_data["boss_line_8"] = "   : /-\ .'-'\ / |"
-        rpg_data["boss_line_9"] = "    \\, ||    \|  "
+        rpg_data["boss_line_9"] = "     \\\, ||    \|  "
         rpg_data["boss_line_10"] ="     \/ ||    ||  "
         return 'CENTAUR INCOMING !!!'
